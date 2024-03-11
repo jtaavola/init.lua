90c85d7c3521f3e636e6d060da2bc78fced889b6
jtaavola <jesse@taavola.dev>
2024-03-10 19:51:16 -0500

    add vim settings

--- lua/jtaavola/plugins/theme.lua
+++ lua/jtaavola/plugins/theme.lua
@@ -1,17 +1,16 @@
 return   {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require('rose-pine').setup({
-			variant = 'moon',
-			highlight_groups = {
-				-- highlight directories in the file explorer
-				Directory = { fg = "rose" },
-			},
-			styles = {
-				italic = false,
-			},
-		})
-		vim.cmd.colorscheme 'rose-pine'
-	end
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        require('rose-pine').setup({
+            variant = 'moon',
+            -- use :h highlight-groups for the built in highlight groups
+            -- use :Inspect to show the treesitter highlight group under the cursor
+            highlight_groups = {
+                -- highlight directories in the file explorer
+                Directory = { fg = "rose" },
+            },
+        })
+        vim.cmd.colorscheme 'rose-pine'
+    end
 }
---
+++ lua/jtaavola/set.lua
@@ -0,0 +1,8 @@
+-- sets indent width to 4 spaces 
+vim.opt.shiftwidth = 4
+-- expand tabs to be spaces
+vim.opt.expandtab = true
+
+-- enable line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
